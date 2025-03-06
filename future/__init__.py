21bbebe187caa45353edfe21f9ed5e4205a00ec0
cptfinch <cptfinch@gmail.com>
2025-03-06 10:43:10 +0000

    Remove future directory as functionality has been migrated to dedicated modules

--- future/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Future enhancements for Jira API Interface.
-
-This package contains modules for planned features that are not yet implemented.
-These are provided as a preview of upcoming functionality and to solicit feedback.
-"""
-
-__version__ = "0.1.0-alpha" 
